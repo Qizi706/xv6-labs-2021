d3b5b66861cc3a61ebb11bcc088995acdb774c87
Celeb <qzhou0706@gmail.com>
2026-04-15 11:44:52 +0800

    FInish Symbolic links

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -309,6 +309,39 @@ sys_open(void)
       return -1;
     }
     ilock(ip);
+    if (ip->type == T_SYMLINK && !(omode & O_NOFOLLOW)){
+      int depth = 0;
+
+      while (depth < 10){
+        char target[MAXPATH];
+
+        if (readi(ip, 0, (uint64)target, 0, MAXPATH) <= 0){
+          iunlockput(ip);
+          end_op();
+          return -1;
+        }
+
+        iunlockput(ip);
+
+        if((ip = namei(target)) == 0){
+          end_op();
+          return -1;
+        }
+
+        ilock(ip);
+        if(ip->type == T_SYMLINK){
+          depth++;
+        } else {
+          break;
+        }
+      }
+
+      if(depth == 10){
+        iunlockput(ip);
+        end_op();
+        return -1;
+      }
+    }
     if(ip->type == T_DIR && omode != O_RDONLY){
       iunlockput(ip);
       end_op();
@@ -482,5 +515,32 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+uint64
+sys_symlink(void)
+{
+  char target[MAXPATH], path[MAXPATH];
+  struct inode *ip;
+
+  if(argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0)
+    return -1;
+
+  begin_op();
+  if((ip = create(path, T_SYMLINK, 0, 0)) == 0){
+    end_op();
+    return -1;
+  }
+
+  int target_len = strlen(target);
+  if (writei(ip, 0, (uint64)target, 0, target_len) != target_len){
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+
+  iunlockput(ip);
+  end_op();
   return 0;
 }
